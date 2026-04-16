bcc12f1af434936a25145ecd91507bfed096a646
vikas sharma <jnv2252@gmail.com>
2026-04-16 20:47:33 +0530

    Add natsort for improved sorting of parameters in Create Parameter scripts

--- PSL Master - MCX/1. Create Paremeter.py
+++ PSL Master - MCX/1. Create Paremeter.py
@@ -1,6 +1,7 @@
 print('Create Parameter')
 import pandas as pd
 from glob import glob
+from natsort import index_natsorted
 code_df = pd.concat([pd.read_csv(p, usecols=['code', 'index', 'dte']) for p in glob('parameters/*.csv')])
 code_df.drop_duplicates(inplace=True)
 code_df.columns = ['Strategy', 'Index', 'dte']
@@ -16,7 +17,7 @@ for index in indices:
         index_rows.append(pd.DataFrame([[index, index, dte, -1, 2, -2]], columns=code_df.columns))
 
 index_df = pd.concat(index_rows, ignore_index=True)
-code_df = code_df.sort_values(by=['Index', 'Strategy', 'dte']).reset_index(drop=True)
+code_df = code_df.iloc[index_natsorted(zip(code_df['Index'], code_df['Strategy'], code_df['dte']))].reset_index(drop=True)
 code_df = pd.concat([index_df, code_df], ignore_index=True)
 code_df.to_csv('MasterParemeter.csv', index=False)
 input('Press Enter to exit :)')
