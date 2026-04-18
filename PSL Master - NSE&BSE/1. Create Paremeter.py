4ae8e6f7182bee04d130a9f5098c2be18830ae7c
vikas sharma <jnv2252@gmail.com>
2026-04-18 16:04:00 +0530

    Refactor parameter creation in Create Parameter scripts to improve readability and efficiency; streamline duplicate handling and index row generation.

--- PSL Master - NSE&BSE/1. Create Paremeter.py
+++ PSL Master - NSE&BSE/1. Create Paremeter.py
@@ -2,21 +2,16 @@ print('Create Parameter')
 import pandas as pd
 from glob import glob
 from natsort import index_natsorted
+
 code_df = pd.concat([pd.read_csv(p, usecols=['code', 'index', 'dte']) for p in glob('parameters/*.csv')])
-code_df.drop_duplicates(inplace=True)
+code_df = code_df.drop_duplicates(subset=['code', 'index', 'dte'])
 code_df.columns = ['Strategy', 'Index', 'dte']
-code_df.insert(3, column='Fund', value=0)
-code_df.insert(4, column='PositivePSL', value=1)
-code_df.insert(5, column='NegativePSL', value=-1)
+code_df[['Fund', 'PositivePSL', 'NegativePSL']] = [0, 1, -1]
 
-index_rows = []
-indices = sorted(code_df['Index'].unique())
-for index in indices:
-    index_dtes = sorted(code_df[code_df['Index'] == index]['dte'].unique())
-    for dte in index_dtes:
-        index_rows.append(pd.DataFrame([[index, index, dte, -1, 2, -2]], columns=code_df.columns))
+index_df = code_df[['Index', 'dte']].drop_duplicates().sort_values(['Index', 'dte']).reset_index(drop=True)
+index_df.insert(0, 'Strategy', index_df['Index'])
+index_df[['Fund', 'PositivePSL', 'NegativePSL']] = [-1, 2, -2]
 
-index_df = pd.concat(index_rows, ignore_index=True)
 code_df = code_df.iloc[index_natsorted(zip(code_df['Index'], code_df['Strategy'], code_df['dte']))].reset_index(drop=True)
 code_df = pd.concat([index_df, code_df], ignore_index=True)
 code_df.to_csv('MasterParemeter.csv', index=False)
